86b1c817989db0dd71f777e441da26f18d095135
LiaLeshchenko <156839096+LiaLeshchenko@users.noreply.github.com>
2024-12-09 19:11:59 +0200

    add style

---
+++ js/script.js
@@ -0,0 +1,27 @@
+console.log(14);
+const age = 14;
+console.log(age);
+
+const name = "Liya";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "я не знаю що написати";
+console.log(myString);
+
+let myNamber = 5;
+myNamber = myNamber + 10;
+console.log(myNamber);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Введи своє імя");
+alert(userName);
+console.log(typeof userName);
+
+const userAnsver = confirm("Видійсно хочете покинути сторінку");
+console.log(userAnsver);
+
